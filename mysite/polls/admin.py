69be26248f36d84869aa022e4734841e5006ddfc
Andy L <oleskainbox@gmail.com>
2026-07-02 17:34:04 -0400

    Templates and render().

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Question
+
+admin.site.site_header = "Polls Admin"
+admin.site.site_title = "The world's slickest Admin Panel"
+admin.site.index_title = "Amazing Title"
 
 # Register your models here.
+admin.site.register(Question)
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 def index(request):
     # processing - db, cache, rendering HTML template
     # print(type(request))
     # print(request.method)
-    print(request.user)
-    return HttpResponse("Hi, world. You're at polls index.")
+    # print(request.user)
+    # return HttpResponse("Hi, world. You're at polls index.")
+
+    # get 5 most recently added questions from the db
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # template = loader.get_template('polls/index.html')
+    # context = {'latest_question_list': latest_question_list}
+    # return HttpResponse(template.render(context, request))
+
+    context = {
+        'latest_question_list': latest_question_list,
+        'numbers': [1,2,3,4,5]
+        }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    respose = "You're looking at the results of question %s."
+    return HttpResponse(respose % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
