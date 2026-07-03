693f3bbda29e3312bd82e58d9c074754606e0632
Andy L <oleskainbox@gmail.com>
2026-07-02 21:21:31 -0400

    HTML forms and POST requests.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 admin.site.site_header = "Polls Admin"
 admin.site.site_title = "The world's slickest Admin Panel"
@@ -7,4 +7,5 @@ admin.site.index_title = "Amazing Title"
 
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Choice)
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import F
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     # processing - db, cache, rendering HTML template
@@ -25,11 +27,50 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+    # try:
+    #     question = Question.objects.get(id=question_id)
+    # except:
+    #     raise Http404("Question does not exist")
+
+    questions = get_list_or_404(Question, id__in=[1,2,3])
+    print(questions)
+
+    question = get_object_or_404(Question, id=question_id)
+
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    respose = "You're looking at the results of question %s."
-    return HttpResponse(respose % question_id)
+    question = get_object_or_404(Question, id=question_id)
+    # respose = "You're looking at the results of question %s."
+    # return HttpResponse(respose % question_id)
+
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # return HttpResponse("You're voting on question %s." % question_id)
+
+    print(request.POST)
+
+    question = get_object_or_404(Question, id=question_id)
+
+    try:
+        choice_pk = request.POST['choice']
+        selected_choice = question.choice_set.get(pk=choice_pk)
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            "question": question,
+            "error_message": "You didn't select a choice." 
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes = F("votes") + 1 
+        # F does the update DIRECTLY on the server, preventing two users to send same value at the same time
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
